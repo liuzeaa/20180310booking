58b4ee02f02f34fc4ea969f1a170566ddb857ee1
liuzeaa <1098367891@qq.com>
2018-03-10 15:30:57 +0800

    用户管理接口修改

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var crypto = require('crypto');
+var pinyin = require('pinyin');
 var User = require('../schemas/user')
 router.get('/',function(req,res,next){
     User.find({isDelected:false},function(err,list){
@@ -10,13 +12,23 @@ router.get('/',function(req,res,next){
 router.get('/create',function(req,res,next){
     res.render('user/createModal',{})
 })
+//只有nickName，name字段作为登录名，pingyin中间件做处理
 router.post('/create',function(req,res,next){
-    User.create(req.body,function(err,doc){
+    var md5 = crypto.createHash("md5");
+   var password = md5.update('123qwe').digest("hex");
+    var name = pinyin(req.body.nickName,{style:pinyin.STYLE_NORMAL}).join(',').replace(/\,+/,'');
+
+    User.create({
+        name:name,
+        nickName:req.body.nickName,
+        password:password
+    },function(err,doc){
         if(err){
             console.log(err.message);
             return;
         }
         console.log(doc)
+        res.redirect('/user')
     })
 })
 //编辑接口
@@ -27,25 +39,34 @@ router.get('/edit/:id',function(req,res,next){
 
 })
 router.post('/edit/:id',function(req,res,next){
-    var data = Object.assign({},req.body,{updatedAt:new Date()});
     User.update({_id:req.params.id},{
-        $set:data
+        $set:{
+            name:name,
+            nickName:req.body.nickName,
+            updatedAt:new Date()
+        }
     },function(err,doc){
         if(err){
-            console.log('edit success')
+            console.log(err.message);
+            return;
         }
+        console.log('edit success')
+        res.redirect('/user')
     })
 })
 //删除接口
 router.get('/user/delete/:id',function(req,res,next){
     User.update({_id:req.params.id},{
         $set:{
-            isDelected:false
+            isDelected:true
         }
     },function(err,doc){
         if(err){
-            console.log('delete success')
+            console.log(err.message);
+            return;
         }
+        console.log('delete success')
+        res.redirect('/user')
     })
 })
 module.exports = router;
