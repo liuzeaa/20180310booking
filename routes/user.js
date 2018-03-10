15fbec547a65e9a7993bb6d438a625c0009adeda
liuzeaa <1098367891@qq.com>
2018-03-10 15:14:04 +0800

    用户管理接口完成

--- routes/user.js
+++ routes/user.js
@@ -6,5 +6,46 @@ router.get('/',function(req,res,next){
         res.render('user/user',{list:list})
     })
 })
+//新增接口
+router.get('/create',function(req,res,next){
+    res.render('user/createModal',{})
+})
+router.post('/create',function(req,res,next){
+    User.create(req.body,function(err,doc){
+        if(err){
+            console.log(err.message);
+            return;
+        }
+        console.log(doc)
+    })
+})
+//编辑接口
+router.get('/edit/:id',function(req,res,next){
+    User.findById(req.params.id,function(err,list){
+        res.render('user/edit',{list:list})
+    })
 
+})
+router.post('/edit/:id',function(req,res,next){
+    var data = Object.assign({},req.body,{updatedAt:new Date()});
+    User.update({_id:req.params.id},{
+        $set:data
+    },function(err,doc){
+        if(err){
+            console.log('edit success')
+        }
+    })
+})
+//删除接口
+router.get('/user/delete/:id',function(req,res,next){
+    User.update({_id:req.params.id},{
+        $set:{
+            isDelected:false
+        }
+    },function(err,doc){
+        if(err){
+            console.log('delete success')
+        }
+    })
+})
 module.exports = router;
